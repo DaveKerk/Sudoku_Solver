a2baa6e80392b8400a5c45cbf857a0b2ce34ab5a
Unknown <davekerk7@gmail.com>
2016-11-05 17:36:49 -0600

    More Working

--- Messing Around 2/main.cpp
+++ Messing Around 2/main.cpp
@@ -4,13 +4,26 @@ using namespace std;
 
 int main()
 {
-        for (int x=0; x < 3; x++)
+    for (int x=0; x < 4; x++)
+    {
+        for (int x = 0; x++; (x==1||x==3||x==5||x==7))
         {
-            for(int x =0; x < 4; x++)
-                    {
-                            cout << setw(10) << "#####" << setw(10);
-                    }
+            for (int x = 0; x < 4; x++)
+            {
+                cout << setw(10) << "#####" << setw(10);
+            }
             cout << endl;
         }
+        for (int x=0; x < 3; x++)
+        {
+            for(int x=0; x++; (x==2||x==4||x==6||x==8))
+            {
+                cout << setw(15) << "#####" <<setw(15);
+
+            }
+            cout << endl;
+
+        }
+    }
     return 0;
 }
